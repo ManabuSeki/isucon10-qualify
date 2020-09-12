5f39e2d9e00284aef117cfd8d5f3d276b4962812
Shoma Okamoto <32533860+shmokmt@users.noreply.github.com>
2020-09-12 15:45:40 +0900

    Add geom.

--- isuumo/webapp/mysql/db/3_IndexEstateData.sql
+++ isuumo/webapp/mysql/db/3_IndexEstateData.sql
@@ -1,1 +1,4 @@
-ALTER TABLE `isuumo`.`estate` ADD INDEX `popularity_idx` (`popularity`);
+ALTER TABLE `isuumo`.`estate` ADD INDEX `popularity_idx` (`popularity`),
+ADD COLUMN `geom` geometry NOT NULL,
+ADD SPATIAL `geom` (`geom`);
+UPDATE `isuumo`.`estate` SET `geom` = ST_GeomFromText(CONCAT('POINT(', `lognitude`, ' ', `latitude`, ')'));
