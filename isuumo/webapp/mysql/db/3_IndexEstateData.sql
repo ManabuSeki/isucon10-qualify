366e20a2bab4a59308572bf5db54943eba044bf8
Shoma Okamoto <32533860+shmokmt@users.noreply.github.com>
2020-09-12 16:35:48 +0900

    緯度経度を逆にする

--- isuumo/webapp/mysql/db/3_IndexEstateData.sql
+++ isuumo/webapp/mysql/db/3_IndexEstateData.sql
@@ -5,6 +5,6 @@ CREATE TABLE IF NOT EXISTS `isuumo`.`geotable` (
   PRIMARY KEY (`id`),
   SPATIAL KEY `geom` (`geom`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-INSERT INTO `isuumo`.`geotable` SELECT `id`, ST_GeomFromText(CONCAT('POINT(', `longitude`, ' ', `latitude`, ')')) 
+INSERT INTO `isuumo`.`geotable` SELECT `id`, ST_GeomFromText(CONCAT('POINT(', `latitude`, ' ', `longnitude`, ')')) 
 FROM `isuumo`.`estate`;
 
