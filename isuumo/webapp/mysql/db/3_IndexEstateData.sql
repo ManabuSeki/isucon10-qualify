2dd6651afb51a043a013fba264e70f32f185fac7
Shoma Okamoto <32533860+shmokmt@users.noreply.github.com>
2020-09-12 18:00:12 +0900

    low_priced_estateの追加

--- isuumo/webapp/mysql/db/3_IndexEstateData.sql
+++ isuumo/webapp/mysql/db/3_IndexEstateData.sql
@@ -6,6 +6,21 @@ CREATE TABLE IF NOT EXISTS `isuumo`.`geotable` (
   PRIMARY KEY (`id`),
   SPATIAL KEY `geom` (`geom`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE IF NOT EXISTS `isuumo`.`low_priced_estate` (
+    id          INTEGER             NOT NULL PRIMARY KEY,
+    name        VARCHAR(64)         NOT NULL,
+    description VARCHAR(4096)       NOT NULL,
+    thumbnail   VARCHAR(128)        NOT NULL,
+    address     VARCHAR(128)        NOT NULL,
+    latitude    DOUBLE PRECISION    NOT NULL,
+    longitude   DOUBLE PRECISION    NOT NULL,
+    rent        INTEGER             NOT NULL,
+    door_height INTEGER             NOT NULL,
+    door_width  INTEGER             NOT NULL,
+    features    VARCHAR(64)         NOT NULL,
+    popularity  INTEGER             NOT NULL
+) ENGINE=InnoDB;
 INSERT INTO `isuumo`.`geotable` SELECT `id`, ST_GeomFromText(CONCAT('POINT(', `latitude`, ' ', `longitude`, ')')) 
 FROM `isuumo`.`estate`;
+INSERT INTO `isuumo`.`low_priced_estate` SELECT * FROM `isuumo`.`estate` ORDER BY rent ASC, id ASC LIMIT 20;
 
