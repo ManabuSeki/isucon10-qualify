7dc4ce4192ef1024646cd9924363e279b6b78458
Shoma Okamoto <32533860+shmokmt@users.noreply.github.com>
2020-09-12 16:09:13 +0900

    fix sql

--- isuumo/webapp/mysql/db/3_IndexEstateData.sql
+++ isuumo/webapp/mysql/db/3_IndexEstateData.sql
@@ -1,5 +1,10 @@
 ALTER TABLE `isuumo`.`estate` ADD INDEX `popularity_idx` (`popularity`),
-ADD COLUMN `geom` geometry;
-UPDATE `isuumo`.`estate` SET `geom` = ST_GeomFromText(CONCAT('POINT(', `longitude`, ' ', `latitude`, ')'));
-ALTER TABLE `isuumo`.`estate` MODIFY COLUMN `geom` geometry NOT NULL,
-ADD SPATIAL `geom` (`geom`);
+CREATE TABLE IF NOT EXISTS `isuumo`.`geotable` (
+  `id`   INTEGER NOT NULL,
+  `geom` geometry NOT NULL,
+  PRIMARY KEY (`id`),
+  SPATIAL KEY `geom` (`geom`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `isuumo`.`geotable` SELECT `id`, ST_GeomFromText(CONCAT('POINT(', `longitude`, ' ', `latitude`, ')')) 
+FROM `issumo`.`estate`;
+
